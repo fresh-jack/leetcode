343461d1a0752cf214a9e3155d61ae741460eefe
guoshuai.wei <jackwgs@126.com>
2023-04-04 13:21:33 +0800

    treeNode

---
+++ algorithms/cpp/tree/tree.h
@@ -0,0 +1,14 @@
+#ifndef _TREE_H_
+#define _TREE_H_
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() :val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val) :val(val), left(nullptr), right(nullptr) {}
+    TreeNode(int val, TreeNode* left, TreeNode* right) :val(val), left(left), right(right) {}
+};
+
+#endif
