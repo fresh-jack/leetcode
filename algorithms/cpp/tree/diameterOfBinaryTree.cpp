634ca606c9a476f7584ddb7a4bb155b5c89692d6
guoshuai.wei <jackwgs@126.com>
2023-04-05 21:24:51 +0800

    diameterOfBinaryTree

---
+++ algorithms/cpp/tree/diameterOfBinaryTree.cpp
@@ -0,0 +1,26 @@
+// Source : https://leetcode.cn/problems/diameter-of-binary-tree/
+// Author : guoshuai wei
+// Date   : 2023-04-03
+// 543
+
+#include "tree.h"
+#include<functional>
+#include<algorithm>
+using namespace std;
+
+int diameterOfBinaryTree(TreeNode* root) {
+    int result = 0;
+    function<int(TreeNode *)> depth = [&](TreeNode *root) -> int
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        int L = depth(root->left);
+        int R = depth(root->right);
+        result = max(result, L + R + 1);
+        return max(L, R) + 1;
+    };
+    depth(root);
+    return result - 1;
+}
