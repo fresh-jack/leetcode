3254d7ebdb31207bf21ed676394ee04aa603a529
jack.wei <wgs491673870@126.com>
2023-04-15 22:12:10 +0800

    maxProfit

---
+++ algorithms/cpp/dynamic/maxProfit.cpp
@@ -0,0 +1,17 @@
+// Source : https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
+// Author : guoshuai wei
+// Date   : 2023-04-15
+// 121
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+    int result = 0;
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            result = max(result, prices[i] - minPrice);
+        }
+    }
+    return result;
+}
