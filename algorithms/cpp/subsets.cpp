d7de2c7bebcdf9f264315107b5242d98da1bbbe7
jack.wei <jackwgs@126.com>
2023-04-17 21:53:34 +0800

    subsets

---
+++ algorithms/cpp/subsets.cpp
@@ -0,0 +1,15 @@
+// Source : https://leetcode.cn/problems/subsets/description/
+// Author : guoshuai wei
+// Date   : 2023-04-03
+// 78
+#include <vector>
+#include <functional>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> path;
+    function<void(vector<int>&, int)> backtracing = [&](vector<int>& nums, int level) {
+
+    }
+}
